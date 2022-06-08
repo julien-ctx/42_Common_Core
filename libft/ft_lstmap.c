0f7b901866333fdb31f43867ba5869cfbf76f8f6
Julien Caucheteux <jcauchet@c2r7p5.42nice.fr>
2022-06-08 17:37:16 +0200

    Added projects

---
+++ libft/ft_lstmap.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juliencaucheteux <juliencaucheteux@stud    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/25 15:16:33 by juliencauch       #+#    #+#             */
+/*   Updated: 2022/06/08 17:30:41 by jcauchet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new_lst;
+	t_list	*last;
+	t_list	*elem;
+	void	*content;
+
+	new_lst = NULL;
+	last = NULL;
+	while (lst)
+	{
+		content = f(lst->content);
+		elem = ft_lstnew(content);
+		if (!elem)
+		{
+			if (new_lst)
+				ft_lstclear(&new_lst, del);
+			return (NULL);
+		}
+		if (last)
+			last->next = elem;
+		if (!new_lst)
+			new_lst = elem;
+		last = elem;
+		lst = lst->next;
+	}
+	return (new_lst);
+}
